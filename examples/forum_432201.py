c1480882f847e3f5485a99248b99380e60ec933e
rhalbersma <rhalbersma@gmail.com>
2019-09-06 14:08:31 +0200

    use np.max()

--- examples/forum_432201.py
+++ examples/forum_432201.py
@@ -8,7 +8,7 @@
 import numpy as np
 import pandas as pd
 
-from gravon import archive, pattern, tidy
+from gravon import archive, tidy
 
 games = pd.read_csv("../data/classic.csv").query('game_fmt == ".xml"')
 setups = tidy.setups(games.copy())
@@ -16,7 +16,7 @@ setups = tidy.add_board(setups)
 setups = tidy.add_WLD_score(setups)
 
 rank_B = 11
-setups['bombed_off_column'] = setups['setup_board'].apply(lambda x: max(np.sum(x.tensor[rank_B,:,:], axis=0)) == 4)
+setups['bombed_off_column'] = setups['setup_board'].apply(lambda x: np.sum(x.tensor[rank_B,:,:], axis=0).max() == 4)
 df = setups.query('bombed_off_column == True')
 df['G'] = 1
 print('{}\n'.format(df[['G', 'W', 'L', 'D']].agg(['sum'])))
