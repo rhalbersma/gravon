7ffabd91e9289c8386e846953337d4d597af86aa
rhalbersma <rhalbersma@gmail.com>
2020-10-21 17:03:00 +0200

    tag patterns with identity and mirrored

--- src/gravon/pattern.py
+++ src/gravon/pattern.py
@@ -5,48 +5,30 @@
 
 import pandas as pd
 
-import scripts.transform.setups as setups
-
-def serial(pat2d: str) -> str:
+def pat1d_identity(pat2d: str) -> str:
     return ''.join([ 
         line.strip() 
         for line in pat2d.splitlines()[::-1] 
     ])
 
-def mirror(pat2d: str) -> str:
+def pat1d_mirrored(pat2d: str) -> str:
     return ''.join([ 
         line.strip()[::-1] 
         for line in pat2d.splitlines()[::-1] 
     ])
 
 def equal(df: pd.DataFrame, pat2d: str, column='setup', mirrored=True) -> pd.DataFrame:
-    pattern_serial = serial(pat2d)
-    equaled_serial = df.query(column + '== @pattern_serial')    
-    if mirrored:
-        pattern_mirror = mirror(pat2d)
-        equaled_mirror = df.query(column + '== @pattern_mirror)')
-    else:
-        equaled_mirror = pd.DataFrame()
     return (pd
         .concat([
-            equaled_serial,
-            equaled_mirror
-        ])
-        .sort_values(['gid', 'player'])
+            df.query(f'{column} == @pat1d_identity(@pat2d)').assign(match_type = 'identity'),
+            df.query(f'{column} == @pat1d_mirrored(@pat2d)').assign(match_type = 'mirrored') if mirrored else pd.DataFrame()
+        ], ignore_index=True)
     )
 
 def match(df: pd.DataFrame, pat2d: str, column='setup', mirrored=True) -> pd.DataFrame:
-    pattern_serial = serial(pat2d)
-    matched_serial = df.query(column + '.str.match(@pattern_serial)')    
-    if mirrored:
-        pattern_mirror = mirror(pat2d)
-        matched_mirror = df.query(column + '.str.match(@pattern_mirror)')
-    else:
-        matched_mirror = pd.DataFrame()
     return (pd
         .concat([
-            matched_serial,
-            matched_mirror
-        ])
-        .sort_values(['gid', 'player'])
+            df.query(f'{column}.str.match(@pat1d_identity(@pat2d))').assign(match_type = 'identity'),
+            df.query(f'{column}.str.match(@pat1d_mirrored(@pat2d))').assign(match_type = 'mirrored') if mirrored else pd.DataFrame()
+        ], ignore_index=True)
     )
