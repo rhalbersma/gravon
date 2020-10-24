afc99c25e6f494158dcc41615829ca0564422511
rhalbersma <rhalbersma@gmail.com>
2020-10-24 23:22:37 +0200

    show row/col sums in heatmap

--- src/gravon/heatmap.py
+++ src/gravon/heatmap.py
@@ -11,20 +11,56 @@ import plotnine as p9
 from gravon.setup import row_labels, col_labels
 
 def heatmap(data: np.array, format_string=None):
-    df = (pd
+    N = data.sum()
+    mu = data.mean()
+    ds = (pd
         .DataFrame(data, index=row_labels, columns=col_labels)
         .rename_axis('row')
         .reset_index()
         .melt(id_vars='row', var_name='col')
-        .assign(log = lambda r: np.log10(r.value))
+        .assign(
+            rel = lambda r: r.value / mu,
+            log = lambda r: np.log10(r.rel)
+        )
+    )
+    rs = (ds
+        .groupby('row')
+        .agg(value = ('value', 'sum'))
+        .reset_index()
+        .assign(
+            col = 'rows',
+            value = lambda r: r.value / N,
+            rel = lambda r: r.value * 4,
+            log = lambda r: np.log10(r.rel)
+        )
+    )
+    cs = (ds
+        .groupby('col')
+        .agg(value = ('value', 'sum'))
+        .reset_index()
+        .assign(
+            row = 'cols',
+            value = lambda r: r.value / N,
+            rel = lambda r: r.value * 10,
+            log = lambda r: np.log10(r.rel)
+        )
+    )
+    df = (pd
+        .concat([ds, rs, cs])
+        .astype(dtype={
+            'row': pd.CategoricalDtype(categories=['1', '2', '3', '4', '', 'cols']),
+            'col': pd.CategoricalDtype(categories=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', '', 'rows'])
+        })
     )
 
-    return (p9.ggplot(df, p9.aes(y='row', x='col', fill='log'))
+    return (p9.ggplot(df, p9.aes(y='row', x='col'))
         # https://stackoverflow.com/a/62161556/819272
         # Plotnine does not support changing the position of any axis.
-        + p9.geom_tile()
         + p9.coord_equal()
+        + p9.geom_tile(p9.aes(fill='log'))
         + p9.geom_text(p9.aes(label='value'), format_string=format_string, size=8)
+        + p9.scale_y_discrete(drop=False)
+        + p9.scale_x_discrete(drop=False)
         + p9.scale_fill_gradientn(colors=['#63BE7B', '#FFEB84', '#F8696B'], guide=False)
-        + p9.theme(axis_ticks=p9.element_blank(), axis_title=p9.element_blank())
+        + p9.theme(axis_ticks=p9.element_blank(), axis_title=p9.element_blank(), panel_grid=p9.element_blank())
     )
